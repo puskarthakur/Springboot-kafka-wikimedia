87ace94fa7e237eefa86bfa2ce3e72d5577e22ec
puskarthakur <puskar1303@gmail.com>
2023-06-05 21:39:17 +0530

    real time practice code

---
+++ src/main/java/com/thakurbrothers/springbootkafkajpa/SpringbootKafkaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.thakurbrothers.springbootkafkajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thakurbrothers/springbootkafkajpa/SpringbootKafkaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thakurbrothers.springbootkafkajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
